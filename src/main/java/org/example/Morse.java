b5526d0c164bcce7b17cd8ca1fa12db274e93bb2
Joar Ekelund <joarek@kth.se>
2020-04-16 13:40:45 +0200

    packages

--- src/main/java/org/example/Morse.java
+++ src/main/java/org/example/Morse.java
@@ -1,3 +1,5 @@
+package org.example;
+
 public class Morse {
 
     public static void main(String[] args) {
@@ -5,9 +7,6 @@ public class Morse {
     }
 
     public static int add(int a, int b) {
-        if(true) {
-            System.out.println("Covered!");
-        }
         return a + b;
     }
 }

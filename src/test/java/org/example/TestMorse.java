50c31e52be8643c3828bb29a32b46f0b3bbd4810
Joar Ekelund <joarek@kth.se>
2020-04-20 10:16:41 +0200

    unsafe func

--- src/test/java/org/example/TestMorse.java
+++ src/test/java/org/example/TestMorse.java
@@ -6,10 +6,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestMorse {
+
     @BeforeClass
     public static void start() {
         Morse.init();
     }
+
     @Test
     public void testWordToMorse() {
         assertEquals("... --- ...", Morse.toMorse("SOS"));

2dc1257089c323dc1cd1fff88fee2378c7b91c20
Joar Ekelund <joarek@kth.se>
2020-04-20 10:25:29 +0200

    removed unsafeFunc

--- src/main/java/org/example/Morse.java
+++ src/main/java/org/example/Morse.java
@@ -108,11 +108,4 @@ public class Morse {
         }
         return sb.toString();
     }
-
-    public static void unsafeFunc() {
-        int[] t = new int[5];
-        for(int i = 0; i < 10; i++) {
-            t[i] = i;
-        }
-    }
 }
